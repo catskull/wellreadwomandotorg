dc486b83eeb7c534f14f3f4cfddbdb361f000995
Carter McBride <18412686+carterworks@users.noreply.github.com>
2020-07-23 17:16:30 -0600

    Tailwindcss (#122)
    * tailwindcss - configure postcss
    
    * tailwindcss - initialize tailwind
    
    * tailwindcss - initialize purgecss
    
    * tailwindcss - basic layout and typography
    
    * tailwindcss - add a dark theme

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  purge: {
+    content: ["./src/**/*.html"],
+    options: {},
+  },
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {},
+  plugins: [],
+};
