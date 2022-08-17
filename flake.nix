0413f38db88d0e9412f24445bc282ceab6d64a44
Carter McBride <18412686+carterworks@users.noreply.github.com>
2022-08-17 14:58:05 -0600

    Switch to nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Carter's personal site";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        inherit (lib) attrValues;
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+      in rec {
+        devShell = with pkgs; mkShell {
+          buildInputs = [ nodejs ];
+        };
+      });
+}
