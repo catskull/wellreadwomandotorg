fc183171d0c59de8710bb6e5f76dcc67509179ad
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-09-05 16:23:06 -0600

    Shrink the size of masto posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
