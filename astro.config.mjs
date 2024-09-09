0cb222f5aa91b8a0214286f409e65f414eedadee
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-09-09 16:50:42 -0600

    Add cloudflare astro integration and Github Actions deployment (#310)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://carter.works",
 	integrations: [sitemap(), tailwind(), icon()],
+	output: "hybrid",
+	adapter: cloudflare({
+		imageService: "passthrough",
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 });
